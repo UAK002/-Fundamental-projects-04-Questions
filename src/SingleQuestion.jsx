ce43690894241c1789d9f81caab93493bd3d3e0f
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-03 22:53:31 +0530

    193. Questions - CSS (Optional)

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 
-const SingleQuestion = ({ title, info }) => {
-  const [showInfo, setShowInfo] = useState(false);
-  //   const [showInfo, setShowInfo] = useState(true);
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
+  // const [showInfo, setShowInfo] = useState(false);
+
+  const isActive = id === activeId;
+
   return (
     <article className="question">
       <header>
@@ -11,14 +13,16 @@ const SingleQuestion = ({ title, info }) => {
         <button
           className="question-btn"
           onClick={() => {
-            setShowInfo(!showInfo);
+            // setShowInfo(!showInfo);
+            toggleQuestion(id);
           }}
         >
-          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          {/* {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />} */}
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {/* <p>{info}</p> */}
-      {showInfo && <p>{info}</p>}
+      {/* {showInfo && <p>{info}</p>} */}
+      {isActive && <p>{info}</p>}
     </article>
   );
 };
